7271b7a4f8eff402fcc22c74971b5717e2e9d2ad
arsh-gupta <56013943+arsh-gupta@users.noreply.github.com>
2019-10-01 23:26:34 +0530

    Update 1B.py

--- 1B.py
+++ 1B.py
@@ -1,5 +1,5 @@
-a=int(input('Enter the number of rows'))
-for i in range(0,a):
+x=int(input('Enter the number of rows: '))
+for i in range(0,x):
     for j in range(0,i+1):
         if(i%2!=0):
             print('0',end="")
